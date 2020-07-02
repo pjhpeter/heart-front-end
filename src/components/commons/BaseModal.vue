7fde7db26b1ce85249109f65c4843f55f3ecbd5a
pjhpeter <pjhpeter@163.com>
2020-07-02 20:40:58 +0800

    基本完成模态框右上角三个按钮的功能，最小化功能还没完善

--- src/components/commons/BaseModal.vue
+++ src/components/commons/BaseModal.vue
@@ -3,12 +3,40 @@
     <Modal
       v-model="isShow"
       :title="title"
-      draggable
-      :width="900"
+      :draggable="isDraggable"
+      :width="1000"
       footer-hide
+      :fullscreen="isFullscreen"
+      :closable="false"
       class-name="base-modal"
-      @on-visible-change="destroy"
     >
+      <template slot="header">
+        <div>
+          <span class="title" v-text="title"></span>
+          <div class="button-group">
+            <Button type="text" class="hide-button" @click="hide"
+              ><i class="iconfont icon-hide"></i
+            ></Button>
+            <Button
+              type="text"
+              v-show="isFullscreen"
+              class="reset-screen-button"
+              @click="resetScreen"
+              ><i class="iconfont icon-reset-screen"></i
+            ></Button>
+            <Button
+              type="text"
+              v-show="!isFullscreen"
+              class="fullscreen-button"
+              @click="fullscreen"
+              ><i class="iconfont icon-fullscreen"></i
+            ></Button>
+            <Button type="text" class="close-button" @click="close"
+              ><i class="iconfont icon-close"></i
+            ></Button>
+          </div>
+        </div>
+      </template>
       <component-loader :menuUrl="menuUrl" />
     </Modal>
   </div>
@@ -41,8 +69,25 @@ export default class BaseModal extends Vue {
   })
   menuUrl!: string;
   isShow = true;
+  isDraggable = true;
+  isFullscreen = false;
 
-  private destroy(): void {
+  private hide(): void {
+    this.isShow = false;
+  }
+
+  private fullscreen(): void {
+    this.isDraggable = false;
+    this.isFullscreen = true;
+  }
+
+  private resetScreen(): void {
+    this.isDraggable = true;
+    this.isFullscreen = false;
+  }
+
+  private close(): void {
+    this.isShow = false;
     this.$destroy();
   }
 }
