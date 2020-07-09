29478a9fb0584be3f9e1633ab7fc9df1474705d1
pjhpeter <pjhpeter@163.com>
2020-07-09 19:58:06 +0800

    重新做了单元测试

---
+++ tests/unit/heart/utils/Commons.spec1.ts
@@ -0,0 +1,65 @@
+// import Menu from "@/model/heart/menu/Menu";
+// import Commons from '@/utils/heart/Commons';
+
+// 改成了私有方法，所以注释掉
+describe("测试doFindMenu方法", () => {
+  //     it("测试单层树", () => {
+  //         let menuTree: Array<Menu> = [
+  //             {
+  //                 menuUrl: "/test1",
+  //                 menuName: "测试模块1"
+  //             },
+  //             {
+  //                 menuUrl: "/test2",
+  //                 menuName: "测试模块2"
+  //             }
+  //         ];
+  //         let menu: Menu | undefined = Commons.doFindMenu(menuTree, "/test1");
+  //         expect(menu).toMatchObject({
+  //             menuUrl: "/test1",
+  //             menuName: "测试模块1"
+  //         });
+  //     });
+  //     it("测试多层树", () => {
+  //         let menuTree: Array<Menu> = [
+  //             {
+  //                 menuUrl: "/test1",
+  //                 menuName: "测试模块1"
+  //             },
+  //             {
+  //                 menuUrl: "/test2",
+  //                 menuName: "测试模块2",
+  //                 children: [{
+  //                     menuUrl: "/test3",
+  //                     menuName: "测试模块3",
+  //                     children: [{
+  //                         menuUrl: "/test4",
+  //                         menuName: "测试模块4"
+  //                     }, {
+  //                         menuUrl: "/test5",
+  //                         menuName: "测试模块5"
+  //                     }]
+  //                 }]
+  //             }
+  //         ];
+  //         let menu: Menu | undefined = Commons.doFindMenu(menuTree, "/test5");
+  //         expect(menu).toMatchObject({
+  //             menuUrl: "/test5",
+  //             menuName: "测试模块5"
+  //         });
+  //     });
+  //     it("测试获取不存在的的节点", () => {
+  //         let menuTree: Array<Menu> = [
+  //             {
+  //                 menuUrl: "/test1",
+  //                 menuName: "测试模块1"
+  //             },
+  //             {
+  //                 menuUrl: "/test2",
+  //                 menuName: "测试模块2"
+  //             }
+  //         ];
+  //         let menu: Menu | undefined = Commons.doFindMenu(menuTree, "/test3");
+  //         expect(menu).toBeUndefined();
+  //     });
+});
